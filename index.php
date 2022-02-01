317e7e6c47cd350f8b2b8b8619ef93c6993c527a
yousafitpro <77118786+yousafitpro@users.noreply.github.com>
2022-02-01 06:42:36 -0800

    ok

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ define('LARAVEL_START', microtime(true));
 |
 | If the application is maintenance / demo mode via the "down" command we
 | will require this file so that any prerendered template can be shown
-| instead of starting the framework, which could cause an exception.
+| instead of starting the framework, which could cause an exception. asasas
 |
 */
 
