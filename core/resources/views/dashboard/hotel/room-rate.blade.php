80fcd377882be839020593254987140832a57400
yousafitpro <77118786+yousafitpro@users.noreply.github.com>
2022-03-10 04:46:33 -0800

    ok

--- core/resources/views/dashboard/hotel/room-rate.blade.php
+++ core/resources/views/dashboard/hotel/room-rate.blade.php
@@ -192,7 +192,7 @@
                         <thead>
                         <th style="background:darkgrey;">Date</th>
                         @foreach($l as $i)
-                            <th style="background:darkgrey;">{{\Carbon\Carbon::parse($i->date)->format("y-m-d")}} {{$i->day}}</th>
+                            <th style="background:darkgrey;">{{\Carbon\Carbon::parse($i->date)->format("y-m-d")}} {{Str::limit($i->day,3,'')}}</th>
                         @endforeach
                         </thead>
                         <tbody>
