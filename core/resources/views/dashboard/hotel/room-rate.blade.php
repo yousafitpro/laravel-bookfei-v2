2d4ea5e7a815594f8259b4a6fce57b7d67c491c0
yousafitpro <77118786+yousafitpro@users.noreply.github.com>
2022-03-15 20:36:00 -0700

    ok

--- core/resources/views/dashboard/hotel/room-rate.blade.php
+++ core/resources/views/dashboard/hotel/room-rate.blade.php
@@ -263,7 +263,7 @@
                             @foreach($l as $i)
                                 <td >
 
-                                        <input  id="checkBox{{$i->id}}"  {{$i->room_price!=null && $i->room_price!='' ?'checked':''}} style="zoom: 1.8" type="checkbox">
+                                        <input  id="checkBox{{$i->id}}"  {{$i->is_disabled==1?'checked':''}} style="zoom: 1.8" type="checkbox">
 
 
 
@@ -355,7 +355,7 @@
                             @foreach($l as $i)
                                 <td >
 
-                                        <input id="checkBox{{$i->id}}"  {{$i->room_price!=null && $i->room_price!='' ?'checked':''}} style="zoom: 1.8" type="checkbox">
+                                        <input id="checkBox{{$i->id}}"  {{$i->is_disabled==1?'checked':''}} style="zoom: 1.8" type="checkbox">
 
 
                                 </td>
