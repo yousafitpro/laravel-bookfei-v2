3ddf9bfe49a4e8511d0de50c82ae4fec186c8646
yousafitpro <77118786+yousafitpro@users.noreply.github.com>
2022-03-17 23:52:01 -0700

    ok

--- core/resources/views/dashboard/hotel/room-rate.blade.php
+++ core/resources/views/dashboard/hotel/room-rate.blade.php
@@ -263,7 +263,7 @@
                             @foreach($l as $i)
                                 <td >
 
-                                        <input {{$i->date==null?'disabled':''}}  id="checkBox{{$i->id}}" onclick="toggleMe('{{$i->id}}')"  {{($i->room_price==null || $i->room_price=='')?'':($i->is_disabled==0?'checked':'')}} style="zoom: 1.8" type="checkbox">
+                                        <input {{($i->date==null|| $i->date<=$rateTable->effective_end_date && $i->date>=$rateTable->effective_start_date)?'disabled':''}}   id="checkBox{{$i->id}}" onclick="toggleMe('{{$i->id}}')"  {{($i->room_price==null || $i->room_price=='')?'':($i->is_disabled==0?'checked':'')}} style="zoom: 1.8" type="checkbox">
 
 
 
@@ -355,7 +355,7 @@
                             @foreach($l as $i)
                                 <td >
 
-                                        <input id="checkBox{{$i->id}}" {{$i->date==null?'disabled':''}} onclick="toggleMe('{{$i->id}}')"  {{($i->room_price==null || $i->room_price=='')?'':($i->is_disabled==0?'checked':'')}} style="zoom: 1.8" type="checkbox">
+                                        <input id="checkBox{{$i->id}}" {{($i->date==null || $i->date<=$rateTable->effective_end_date && $i->date>=$rateTable->effective_start_date)?'disabled':''}} onclick="toggleMe('{{$i->id}}')"  {{($i->room_price==null || $i->room_price=='')?'':($i->is_disabled==0?'checked':'')}} style="zoom: 1.8" type="checkbox">
 
 
                                 </td>
